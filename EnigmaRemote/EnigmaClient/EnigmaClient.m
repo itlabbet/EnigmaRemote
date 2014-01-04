1d2c62721f18447f6ab7cf934aef4020a3c49970
Niklas Andersson <niklasandersson@Niklass-MacBook-Pro.local>
2014-01-04 15:48:26 +0100

    Updated http api comments

--- EnigmaRemote/EnigmaClient/EnigmaClient.m
+++ EnigmaRemote/EnigmaClient/EnigmaClient.m
@@ -6,6 +6,41 @@
 //  Copyright (c) 2014 Niklas Andersson. All rights reserved.
 //
 
+// API for the http communication with dreambox 800hd se running Enigma 2
+
+// State
+// http://192.168.10.12/web/powerstate            // Get powerstate
+// http://192.168.10.12/web/powerstate?newstate=0 // Toggle stand by
+// http://192.168.10.12/web/powerstate?newstate=1 // Shut down hard
+// http://192.168.10.12/web/powerstate?newstate=2 // Reboot
+// http://192.168.10.12/web/powerstate?newstate=3 // Restart GUI
+
+// About your box
+// http://192.168.10.12/web/about
+// http://192.168.10.12/web/deviceinfo
+
+// Current local time
+// http://192.168.10.12/web/currenttime
+
+// Bouquets
+// http://192.168.10.12/web/getservices
+
+// URL för Svenska kanaler
+// http://192.168.10.12/web/getservices?sRef=1:7:1:0:0:0:0:0:0:0:FROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet
+
+// EPG för SVT2 HD
+// http://192.168.10.12/web/epgservice?sRef=1:0:19:416:36:A027:FFFF0000:0:0:0:
+
+// Byta kanal till SVT 2 HD
+// http://192.168.10.12/web/zap?sRef=1:0:19:416:36:A027:FFFF0000:0:0:0:
+
+// Byta kanal till SVT1
+// http://192.168.10.12/web/zap?sRef=1:0:1:449:2D:A027:FFFF0000:0:0:0:
+
+// Tittar på just nu
+// http://192.168.10.12/web/getcurrent
+
+
 #import "EnigmaClient.h"
 #import "XMLReader.h"
 
@@ -89,34 +124,7 @@
     return nil;
 }
 
-// State
-// http://192.168.10.12/web/powerstate
-// http://192.168.10.12/web/powerstate?newstate=0 // Toggle stand by
-// http://192.168.10.12/web/powerstate?newstate=1 // Shut down hard
-// http://192.168.10.12/web/powerstate?newstate=2 // Reboot
-// http://192.168.10.12/web/powerstate?newstate=3 // Restart GUI
 
-// About your box
-// http://192.168.10.12/web/about
-// http://192.168.10.12/web/deviceinfo
-
-// Current local time
-// http://192.168.10.12/web/currenttime
-
-// URL för Svenska kanaler
-// http://192.168.10.12/web/getservices?sRef=1:7:1:0:0:0:0:0:0:0:FROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet
-
-// EPG för SVT2 HD
-// http://192.168.10.12/web/epgservice?sRef=1:0:19:416:36:A027:FFFF0000:0:0:0:
-
-// Byta kanal till SVT 2 HD
-// http://192.168.10.12/web/zap?sRef=1:0:19:416:36:A027:FFFF0000:0:0:0:
-
-// Byta kanal till SVT1
-// http://192.168.10.12/web/zap?sRef=1:0:1:449:2D:A027:FFFF0000:0:0:0:
-
-// Tittar på just nu
-// http://192.168.10.12/web/getcurrent
 
 @end
 
