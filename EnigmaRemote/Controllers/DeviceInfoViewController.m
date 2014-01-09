c47853ac48a15c00f24728823ec01a9cc2153cc2
Niklas Andersson <niklasandersson@Niklass-MacBook-Pro.local>
2014-01-09 23:23:58 +0100

    Added more views

---
+++ EnigmaRemote/Controllers/DeviceInfoViewController.m
@@ -0,0 +1,22 @@
+//
+//  DeviceInfoViewController.m
+//  EnigmaRemote
+//
+//  Created by Niklas Andersson on 09/01/14.
+//  Copyright (c) 2014 Niklas Andersson. All rights reserved.
+//
+
+#import "DeviceInfoViewController.h"
+
+@interface DeviceInfoViewController ()
+
+@end
+
+@implementation DeviceInfoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
